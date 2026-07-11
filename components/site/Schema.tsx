9efb6bdf912ed4ec7b1469bcb248d147ba21f561
nabil <nidatiktoker1@gmail.com>
2026-07-11 15:48:39 +0500

    Add BreadcrumbSchema export

--- components/site/Schema.tsx
+++ components/site/Schema.tsx
@@ -90,3 +90,28 @@ export function FAQSchema({ faqs }: { faqs: FAQItem[] }) {
     />
   );
 }
+
+interface BreadcrumbItem {
+  name: string;
+  url: string;
+}
+
+export function BreadcrumbSchema({ items }: { items: BreadcrumbItem[] }) {
+  const data = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: items.map((item, i) => ({
+      '@type': 'ListItem',
+      position: i + 1,
+      name: item.name,
+      item: item.url,
+    })),
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
